04b7ddc8e050009eafdf336d67a91a617b53f1d5
vivlonglz <vivlonglz@gmail.com>
2017-02-27 16:59:57 +0800

    Update

--- home-about.php
+++ home-about.php
@@ -1,36 +1,81 @@
-<!-- about section -->
-<?php $wl_theme_options = kadima_get_options(); ?>
-<section id="what-we-do" class="what-we-do " style="background-color: #ececec;;">
-			<div class="container">
-					<div class="row">
-							<div class="col-md-12 col-md-offset-1" style="margin-left: 0;padding-top:4em;padding-bottom:4em;">
-									<div class="row">
-									<?php for($i=1; $i<=4; $i++ ) { ?>
-									<?php if($i==1||$i==3) { ?>
-									<div class="col-md-6" style="border-right: 1px solid #d8d8d8;text-align: center;"></div<
-									<?php } ?>
-											
-											<div class="col-md-6 col-sm-6 col-xs-12" style="pading-left:0;pading-right:0;">	
-												
-													<div class="do-box text-center" >
-															
-															<?php if($wl_theme_options['custom_img_1_'.$i] !='') { ?>
-																<i class="lnr"><img style="float:right;" src="<?php echo esc_url($wl_theme_options['custom_img_1_'.$i]); ?>" style="height: 60px;"></i>
-															<?php } ?>
-													
-															<div style="float: right;text-align: right;padding-top: 10px;padding-right: 20px;width: 80%;">
-																<?php if($wl_theme_options['custom_title_1_'.$i] !='') { ?><h3 style="font-size: 18px;font-weight: bold;margin-top:0 !important;"><?php echo esc_attr($wl_theme_options['custom_title_1_'.$i]); ?></h3><?php } ?>
-																<?php if($wl_theme_options['custom_text_1_'.$i] !='') { ?><p style="font-size: 16px"><?php echo apply_filters('the_content', $wl_theme_options['custom_text_1_'.$i], true); ?></p><?php } ?>
-															</div>
-															<a href="<?php echo esc_url($wl_theme_options['custom_link_1_'.$i]); ?>">
-																<!--<?php if($wl_theme_options['about_slide_btn_text_'.$i] !='') { ?><p><?php echo apply_filters('the_content', $wl_theme_options['about_slide_btn_text_'.$i], true); ?></p><?php } ?>-->
-															</a>
-													</div>	
-											</div>
-										<?php }?>
-									</div>
-							</div>
+<?php $theme_options = kadima_get_options(); ?>
+<div class="content-grid">		
+	<div class="col-md-12 product-bj" style="padding-top: 20px;padding-bottom: 20px;background-color: #ececec;">
+		<div class="container">
+			<h3 style="text-align: center;font-size: 55px;color: #ff7f02;font-weight: 100;margin-bottom: 20px;">
+				<?php echo $theme_options['custom_title_1']; ?>
+			</h3>
+			<h5>
+				<?php echo $theme_options['custom_title_sub_1']; ?>
+			</h5>
+			<div class="col-md-12 box_2" >					
+				<div>
+					<div class="most-1">
+						<h6 style="text-align:center;margin-bottom: 30px;font-size: 16px;padding:0 45px;">
+							<?php echo esc_attr($theme_options['custom_desciption_1']); ?>
+						</h6>
 					</div>
+				</div>
 			</div>
-	</section>
+			<p class="p1" style="text-align:center;">
+				<img src="//xmcolorbaby.site.yunclever.com/wp-content/themes/kadima/images/sp.jpg">
+				<!--img class=" zoom-img" src="<?php echo esc_url($theme_options['port_img_'.$i]); ?>" alt="<?php echo esc_attr($theme_options['port_title_'.$i]); ?>"-->
+			</p>
+		</div>
+	</div>
 </div>
+<section style="background-color: #ececec;;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12" style="padding-top:4em;padding-bottom:4em;">
+				<div class="row">
+					<?php for($i=1; $i<=4; $i++ ) { ?>
+					<?php if($i==1||$i==3) { ?>
+						<div class="col-md-6" style="border-right: 1px dashed #00a0e9; padding:20px;">
+					<?php } else { ?>
+						<div class="col-md-6" style="padding:20px;">	
+					<?php } ?>												
+							<div>															
+								<?php if($theme_options['custom_img_1_'.$i] !='') { ?>
+									<?php if($i==1||$i==3) { ?>
+										<div class="pull-right">
+											<div class="col-md-1"></div>
+											<i class="col-md-2">
+												<img src="<?php echo esc_url($theme_options['custom_img_1_'.$i]); ?>">
+											</i>
+											<div class="col-md-1"></div>
+										</div>
+										<div class="col-md-8 text-right">
+											<h3 style="font-size: 18px; font-weight: bold; color:<?php echo esc_attr($theme_options['custom_title_color_1_'.$i]); ?>;">
+												<?php echo esc_attr($theme_options['custom_title_1_'.$i]); ?>
+											</h3>
+											<p style="font-size: 16px">
+												<?php echo apply_filters('the_content', $theme_options['custom_text_1_'.$i], true); ?>
+											</p>
+										</div>
+									<?php } else { ?>
+										<div class="pull-left">
+											<div class="col-md-1"></div>
+											<i class="col-md-2">
+												<img src="<?php echo esc_url($theme_options['custom_img_1_'.$i]); ?>">
+											</i>
+											<div class="col-md-1"></div>
+										</div>
+										<div class="col-md-8 pull-right text-left">
+											<h3 style="font-size: 18px; font-weight: bold; color:<?php echo $theme_options['custom_title_color_1_'.$i]; ?>;">
+												<?php echo esc_attr($theme_options['custom_title_1_'.$i]); ?>
+											</h3>
+											<p style="font-size: 16px">
+												<?php echo apply_filters('the_content', $theme_options['custom_text_1_'.$i], true); ?>
+											</p>
+									</div>
+									<?php } ?>									
+								<?php } ?>
+							</div>	
+						</div>
+					<?php }?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- home-portfolio.php
+++ home-portfolio.php
@@ -6,11 +6,11 @@
 				<h3 style="text-align: center;font-size: 55px;color: #ff7f02;font-weight: 100;margin-bottom: 20px;"><?php echo esc_attr($wl_theme_options['port_heading']); ?></h3>
 				<?php for($i=1 ; $i<=1; $i++) { ?>
 					<div class="col-md-12 box_2" >					
-							<div>
+						<div>
 							<div class="most-1">
-									<!--<h5><a href="<?php echo esc_url($wl_theme_options['port_link_'.$i]); ?>"><?php echo esc_attr($wl_theme_options['port_title_'.$i]); ?></a></h5>-->
-									<h6 style="text-align:center;margin-bottom: 30px;font-size: 16px;padding:0 45px;"><?php echo esc_attr($wl_theme_options['port_description_'.$i]); ?></h6>
-									<!--<p><a href="<?php echo esc_url($wl_theme_options['port_link_'.$i]); ?>">READ MORE ></a></p>-->
+								<!--<h5><a href="<?php echo esc_url($wl_theme_options['port_link_'.$i]); ?>"><?php echo esc_attr($wl_theme_options['port_title_'.$i]); ?></a></h5>-->
+								<h6 style="text-align:center;margin-bottom: 30px;font-size: 16px;padding:0 45px;"><?php echo esc_attr($wl_theme_options['port_description_'.$i]); ?></h6>
+								<!--<p><a href="<?php echo esc_url($wl_theme_options['port_link_'.$i]); ?>">READ MORE ></a></p>-->
 							</div>
 							<?php for($i=1 ; $i<=2; $i++) { ?>
 							<p style="text-align:center;">
@@ -19,9 +19,9 @@
 							</a>
 							<?php } ?>
 							</p>
-							</div>
+						</div>
 					</div>
-					 <p class="p1" style="text-align:center;"><img src="../wp-content/themes/YunBox-kabeile/images/sp.jpg"></p>
+					 <p class="p1" style="text-align:center;"><img src="../wp-content/themes/kadima/images/sp.jpg"></p>
 				<?php } ?>
 				<div class="clearfix"> </div>
 			</div>
